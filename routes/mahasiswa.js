31c732bc094e5d0fb5a61fa6c5032bce4b850676
sahatilfat <sahatilfat@gmail.com>
2021-07-03 21:01:47 +0700

    cara membuat route crud sederhana

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -14,4 +14,32 @@ router.post("/", (req, res, next) => {
   });
 });
 
+router.get("/:nim", (req, res, next) => {
+  const nim = req.params.nim;
+  if (nim === "12345") {
+    res.status(200).json({
+      message: "NIM 12345",
+    });
+  } else {
+    res.status(200).json({
+      message: "NIM LAIN",
+    });
+  }
+  res.status(200).json({
+    message: "Get Method mahasiswa",
+  });
+});
+
+router.put("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Put Method mahasiswa",
+  });
+});
+
+router.delete("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete Method mahasiswa",
+  });
+});
+
 module.exports = router;
