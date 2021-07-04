ca51220e5f028ca3ce001962386706ac6194dcd4
sahatilfat <sahatilfat@gmail.com>
2021-07-04 11:21:20 +0700

    handling error

--- app.js
+++ app.js
@@ -12,4 +12,21 @@ app.use(bodyParse.json());
 
 app.use("/mahasiswa", mahasiswaRoutes);
 
+// jika ada route yg belum didefinisikan, maka akan di arahkan ke function berikut ini:
+app.use((req, res, next) => {
+  const error = new Error("Tidak ditemukan");
+  error.status = 404;
+  // lalu kita definikan function error ini, utk function yg akan kita tulis berikutnya
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 module.exports = app;
